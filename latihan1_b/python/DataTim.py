633a2bd0dd65e71d55f4e7025664fddf9a6d1a03
joynelly <99189201+joynelly@users.noreply.github.com>
2022-02-15 01:17:46 +0700

    Update DataTim.py

--- latihan1_b/python/DataTim.py
+++ latihan1_b/python/DataTim.py
@@ -5,11 +5,14 @@ class DataTim():
     __negaraAsal = ""
     __tahunBerdiri = ""
 
+    # constructor
     def __init__(self, nt="", na="", tb=0):
         self.__namaTim = nt
         self.__negaraAsal = na
         self.__tahunBerdiri = tb
+        self.DaftarPemain = self.DataPemain()
     
+    # Membuat set dan get untuk setiap variable
     def setNamaTim(self, nt):
         self.__namaTim = nt
 
@@ -27,5 +30,40 @@ class DataTim():
     
     def getTahunBerdiri(self):
         return self.__tahunBerdiri
+
+    def setPemain(self, np):
+        self.DaftarPemain.setNamaPemain(np)
+
+    def setJumlahPemain(self, jp):
+        self.DaftarPemain.setJumlah(jp)
+
+    # Menampilkan data Tim termasuk nama pemain
+    def showDataTim(self):
+        print("\n>> " + str(self.__namaTim) + " <<")
+        print("Negara Asal   : " + str(self.__negaraAsal))
+        print("Tahun Berdiri : " + str(self.__tahunBerdiri))
+        print("Daftar Pemain :")
+        self.DaftarPemain.showDataPemain()
     
-    
+    # inner class
+    class DataPemain():
+        #private atribute
+        namaPemain = []
+        jumlah = 0
+
+        # constructor
+        def __init__(self):
+            self.namaPemain = []
+            self.jumlah = 0
+        
+        def setNamaPemain(self, np):
+            self.namaPemain.append(np)
+
+        def setJumlah(self, jp):
+            self.jumlah = jp
+        
+        # menampilkan nama setiap pemain
+        def showDataPemain(self):
+            i = 0
+            for i in range(self.jumlah):
+                print("   " + str(i+1) + ". " + str(self.namaPemain[i]))
